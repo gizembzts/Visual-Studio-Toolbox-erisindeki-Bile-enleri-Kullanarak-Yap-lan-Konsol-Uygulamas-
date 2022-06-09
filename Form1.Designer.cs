7939f616829c4cf658a2ed4732b8e8b874b2b268
gizembzts <105049232+gizembzts@users.noreply.github.com>
2022-06-09 21:42:26 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿
+namespace görselprogramlamaproje
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtIsım = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtTelefon = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.comboBoxSehirler = new System.Windows.Forms.ComboBox();
+            this.rButtonKadin = new System.Windows.Forms.RadioButton();
+            this.rButtonErkek = new System.Windows.Forms.RadioButton();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.BtnKaydol = new System.Windows.Forms.Button();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtIsım
+            // 
+            this.txtIsım.Location = new System.Drawing.Point(114, 40);
+            this.txtIsım.Name = "txtIsım";
+            this.txtIsım.Size = new System.Drawing.Size(121, 23);
+            this.txtIsım.TabIndex = 0;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(114, 85);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(121, 23);
+            this.txtEmail.TabIndex = 0;
+            // 
+            // txtTelefon
+            // 
+            this.txtTelefon.Location = new System.Drawing.Point(114, 132);
+            this.txtTelefon.Name = "txtTelefon";
+            this.txtTelefon.Size = new System.Drawing.Size(121, 23);
+            this.txtTelefon.TabIndex = 0;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(114, 182);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(121, 23);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // comboBoxSehirler
+            // 
+            this.comboBoxSehirler.FormattingEnabled = true;
+            this.comboBoxSehirler.Location = new System.Drawing.Point(114, 232);
+            this.comboBoxSehirler.Name = "comboBoxSehirler";
+            this.comboBoxSehirler.Size = new System.Drawing.Size(121, 23);
+            this.comboBoxSehirler.TabIndex = 2;
+            this.comboBoxSehirler.Text = "Şehir Seçiniz";
+            // 
+            // rButtonKadin
+            // 
+            this.rButtonKadin.AutoSize = true;
+            this.rButtonKadin.Location = new System.Drawing.Point(114, 284);
+            this.rButtonKadin.Name = "rButtonKadin";
+            this.rButtonKadin.Size = new System.Drawing.Size(55, 19);
+            this.rButtonKadin.TabIndex = 4;
+            this.rButtonKadin.TabStop = true;
+            this.rButtonKadin.Text = "Kadın";
+            this.rButtonKadin.UseVisualStyleBackColor = true;
+            // 
+            // rButtonErkek
+            // 
+            this.rButtonErkek.AutoSize = true;
+            this.rButtonErkek.Location = new System.Drawing.Point(220, 284);
+            this.rButtonErkek.Name = "rButtonErkek";
+            this.rButtonErkek.Size = new System.Drawing.Size(53, 19);
+            this.rButtonErkek.TabIndex = 4;
+            this.rButtonErkek.TabStop = true;
+            this.rButtonErkek.Text = "Erkek";
+            this.rButtonErkek.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(109, 331);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(101, 19);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Kitap Okumak";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(231, 330);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(95, 19);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.Text = "Spor Yapmak";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(109, 357);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(106, 19);
+            this.checkBox3.TabIndex = 5;
+            this.checkBox3.Text = "Bisiklet Sürmek";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(231, 356);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(126, 19);
+            this.checkBox4.TabIndex = 6;
+            this.checkBox4.Text = "Enstürman Çalmak";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // BtnKaydol
+            // 
+            this.BtnKaydol.Location = new System.Drawing.Point(160, 422);
+            this.BtnKaydol.Name = "BtnKaydol";
+            this.BtnKaydol.Size = new System.Drawing.Size(107, 34);
+            this.BtnKaydol.TabIndex = 7;
+            this.BtnKaydol.Text = "Kaydol";
+            this.BtnKaydol.UseVisualStyleBackColor = true;
+            this.BtnKaydol.Click += new System.EventHandler(this.BtnKaydol_Click);
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 15;
+            this.listBox.Location = new System.Drawing.Point(536, 65);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(208, 319);
+            this.listBox.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 15);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Ad - Soyad";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 15);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Email";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 15);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Telefon";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 188);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Doğum Tarihi";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 240);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Şehir";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(27, 286);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 15);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Cinsiyet";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(27, 331);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 15);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Hobiler";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(536, 23);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(65, 15);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Kullanıcılar";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(310, 65);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(146, 162);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(679, 23);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(65, 15);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Kullanıcılar";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(773, 544);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.BtnKaydol);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.rButtonErkek);
+            this.Controls.Add(this.rButtonKadin);
+            this.Controls.Add(this.comboBoxSehirler);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.txtTelefon);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtIsım);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIsım;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtTelefon;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox comboBoxSehirler;
+        private System.Windows.Forms.RadioButton rButtonKadin;
+        private System.Windows.Forms.RadioButton rButtonErkek;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Button BtnKaydol;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace görselprogramlamaproje
+{
+    public partial class Form1 : Form
+    {        
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnKaydol_Click(object sender, EventArgs e)
+        {
+            // formda girilecek değerleri tutmak için değişkenler tanımlandı
+            string adSoyad, email, telefonNo, dogumTarihi, cinsiyet, sehir;
+            List<String> hobiler=new List<string>();
+
+            // form elemanlarına girilen değerler değişkenlere atandı
+            adSoyad = txtIsım.Text;
+            email = txtEmail.Text;
+            telefonNo = txtTelefon.Text;
+            dogumTarihi = dateTimePicker1.Text;
+
+            if (rButtonErkek.Checked == true)
+            {
+                cinsiyet = "Erkek";
+            }
+            else if(rButtonKadin.Checked==true)           
+            {
+                cinsiyet = "Kadın";
+            }
+            else
+            {
+                cinsiyet = " ";
+            }
+
+            sehir = comboBoxSehirler.SelectedItem.ToString();
+
+            if (checkBox1.Checked == true)
+            {
+                hobiler.Add(checkBox1.Text);
+            }
+            if (checkBox2.Checked == true)                
+            {
+                hobiler.Add(checkBox2.Text);
+            }
+            if (checkBox3.Checked == true)
+            {
+                hobiler.Add(checkBox3.Text);
+            }
+            if (checkBox4.Checked == true)
+            {
+                hobiler.Add(checkBox4.Text);
+            }
+
+            string hobies = "";
+
+            foreach(var hobi in hobiler)
+            {
+                hobies = hobies + "\n" + hobi.ToString();
+            }
+                      
+            // Girilen kullanıcının isim ve telefon bilgisi listBox'a yazdırıldı
+            listBox.Items.Add(adSoyad + " - " + telefonNo);
+
+            // Form elemanları temizlendi
+            txtIsım.Clear();
+            txtTelefon.Clear();
+            txtEmail.Clear();
+            rButtonErkek.Checked = false;
+            rButtonKadin.Checked = false;           
+            comboBoxSehirler.SelectedItem = null;
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            checkBox3.Checked = false;
+            checkBox4.Checked = false;
+
+            // Girilen veriler messageBox ile gösterildi
+
+            MessageBox.Show("İsim: " + adSoyad + "\nEmail: " + email +
+                "\nTelefon: "+ telefonNo + "\nSehir: " + sehir + "\nCinsiyet: " + 
+                cinsiyet + "\nDogum Tarihi:  " + dogumTarihi +
+                "\nHobiler: " +hobies);
+        }
+
+        int dakika = 0, saniye = 0;
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            // Kronometre için timer oluşturuldu.             
+            // Her 60 saniyede dakika değeri 1 arttırılıyor.
+            if (saniye == 60)
+            {
+                saniye = 0;
+                dakika++;
+            }
+            label9.Text = String.Format("{0:D2}", dakika) + ":" + String.Format("{0:D2}", saniye);
+            saniye++; 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            timer1.Start(); // Form yüklendiğinde timer başlatılıyor. Interval = 1000 (Her 1 saniyede tekrar) 
+
+            // picturBox'a resim url'si girilerek görsel atanıyor
+            pictureBox1.ImageLocation = "https://cdn-icons-png.flaticon.com/512/456/456212.png";
+
+            // ComboBox'a şehir isimleri eklenmekte
+            comboBoxSehirler.Items.Add("İstanbul");
+            comboBoxSehirler.Items.Add("Ankara");
+            comboBoxSehirler.Items.Add("Kocaeli");
+            comboBoxSehirler.Items.Add("Bursa");
+            comboBoxSehirler.Items.Add("İzmir");
+            comboBoxSehirler.Items.Add("Tokat");
+            comboBoxSehirler.Items.Add("Mersin");
+            comboBoxSehirler.Items.Add("Malatya");
+            comboBoxSehirler.Items.Add("Burdur");
+            comboBoxSehirler.Items.Add("Bilecik");
+            comboBoxSehirler.Items.Add("Manisa");
+            comboBoxSehirler.Items.Add("Afyon");
+
+        }
+    }
+}
